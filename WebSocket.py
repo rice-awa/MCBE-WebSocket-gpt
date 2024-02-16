949e2c38d4c667e7f3f2bfd10654806ca6e95773
rice-awa <141110807+rice-awa@users.noreply.github.com>
2024-02-16 09:46:55 +0800

    Update WebSocket.py

--- WebSocket.py
+++ WebSocket.py
@@ -4,7 +4,7 @@ import websockets
 from gptapi import GPTAPIConversation
 
 # 请修改此处"API_URL"和"API_KEY"
-api_url = "API_URL" # API地址
+api_url = "API_URL" # API地址 #例：https://chat.openai.com/v1/chat/completions
 api_key = "API_KEY"  # 硬编码api用于本地测试
 
 system_prompt = "请始终保持积极和专业的态度。回答尽量保持一段话不要太长，适当添加换行符" # 系统提示词
